6ba4bfe1325a87f061cd6c5d8cc9bf0f7cb327c9
Midas van Soest <midasvsoest@gmail.com>
2025-05-21 11:10:06 +0200

    Update: Weapon Model

--- models/Items/Weapon.model.js
+++ models/Items/Weapon.model.js
@@ -3,12 +3,73 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const weaponSchema = new Schema({
-    damage: { type: String,
-        enum: ['Slashing', 'Piercing', 'Bludgeoning'],
-        required: true},
-    range: String,
-    weaponType: { type: String, enum: ['Melee', 'Ranged', 'Thrown', 'Magic'] },
-    weight: { type: Number },
+  weaponName: { type: String, required: true },
+  weaponType: {
+    type: String,
+    required: true,
+    enum: [
+      'Greataxe', 'Dagger', 'Rapier', 'Scimitar', 'Longsword',
+      'Shortsword', 'Mace', 'Greatsword', 'Warhammer', 'Bow',
+      'Crossbow', 'Staff', 'Spear', 'Club', 'Halberd', 'Light Hammer', 'Sickle', 'Other'
+    ]
+  },
+  weaponAttribute: {
+    type: String,
+    enum: ['Melee', 'Ranged', 'Thrown', 'Magic'],
+    required: true
+  },
+  weaponBonus: {
+    type: Number,
+    enum: [1, 2, 3, 4, 5],
+    required: false
+  },
+  damageTypes: [
+    {
+      type: {
+        type: String,
+        enum: [
+          'Slashing', 'Piercing', 'Bludgeoning', 'Fire', 'Cold',
+          'Acid', 'Necrotic', 'Radiant', 'Poison', 'Psychic',
+          'Thunder', 'Force', 'Lightning'
+        ],
+        required: true
+      },
+      die: {
+        type: String,
+        required: true,
+        match: /^\d+d\d+$/ // e.g., 1d6
+      }
+    }
+  ],
+  weaponMastery: {
+    type: String,
+    enum: ['Cleave', 'Nick', 'Vex', 'Push', 'Slow', 'Flex', 'None'],
+    default: 'None'
+  },
+  range: String,
+  weight: Number
+});
+
+// Weapon Mastery Mapping
+const masteryMap = {
+  Cleave: ['Greataxe', 'Halberd'],
+  Nick: ['Dagger', 'Light Hammer', 'Sickle', 'Scimitar'],
+  Vex: ['Rapier', 'Shortsword'],
+  Push: ['Warhammer', 'Mace'],
+  Slow: ['Greatsword'],
+  Flex: ['Longsword', 'Spear']
+};
+
+// Auto-assign weaponMastery based on weaponType
+weaponSchema.pre('save', function (next) {
+  for (const [mastery, weapons] of Object.entries(masteryMap)) {
+    if (weapons.includes(this.weaponType)) {
+      this.weaponMastery = mastery;
+      return next();
+    }
+  }
+  this.weaponMastery = 'None';
+  next();
 });
 
 module.exports = Item.discriminator('Weapon', weaponSchema);
