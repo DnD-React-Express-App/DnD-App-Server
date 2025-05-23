b975567e85e763442ef91458f48ef7e0ef91c5ed
Dreng0t <louischicot4@gmail.com>
2025-05-23 10:50:28 +0100

    added greater validation to some of the spell fields with enum and added fields for Atk/Save

--- models/Spell.model.js
+++ models/Spell.model.js
@@ -4,12 +4,54 @@ const { Schema } = mongoose;
 const spellSchema = new Schema({
   name: { type: String, required: true },
   level: { type: Number, required: true },
-  school: String,
+  school: {
+    type: String,
+    enum: [
+      'Abjuration', 'Conjuration', 'Divination',
+      'Enchantment', 'Evocation', 'Illusion',
+      'Necromancy', 'Transmutation'
+    ],
+  },
   castingTime: String,
   range: String,
-  components: [String],
-  duration: String,
+  components: {
+    type: [String],
+    enum: ['V', 'S', 'M'],
+  },
+  duration: {
+    type: String,
+    enum: [
+      'Instantaneous',
+      'Concentration, up to 1 minute',
+      'Concentration, up to 10 minutes',
+      '1 minute',
+      '10 minutes',
+      '1 hour',
+      '8 hours',
+      '24 hours',
+      'Until dispelled'
+    ],
+  },
   description: String,
+  attackSave: {
+    type: String,
+    enum: [
+      '',
+      'Melee', 'Ranged', 'Spell Attack',
+      'STR Save', 'DEX Save', 'CON Save',
+      'INT Save', 'WIS Save', 'CHA Save'
+    ]
+  },
+  damageEffect: {
+    type: String,
+    enum: [
+      '',
+      'Acid', 'Bludgeoning', 'Cold', 'Fire', 'Force',
+      'Lightning', 'Necrotic', 'Piercing', 'Poison',
+      'Psychic', 'Radiant', 'Slashing', 'Thunder',
+      'Control', 'Buff', 'Debuff', 'Utility'
+    ]
+  }
 });
 
 module.exports = mongoose.model('Spell', spellSchema);
