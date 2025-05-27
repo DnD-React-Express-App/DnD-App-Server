85790441ba9f6d3b60863a7e78e986de5838e069
Midas van Soest <midasvsoest@gmail.com>
2025-05-27 16:35:26 +0200

    Add subclass

--- models/Character.model.js
+++ models/Character.model.js
@@ -2,100 +2,82 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const characterSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  classes: [{
     name: {
-        type: String,
-        required: true
-    },
-    classes: [{
-        name: {
-            type: String,
-            enum: [
-                'Barbarian',
-                'Bard',
-                'Cleric',
-                'Druid',
-                'Fighter',
-                'Monk',
-                'Paladin',
-                'Ranger',
-                'Rogue',
-                'Sorcerer',
-                'Warlock',
-                'Wizard'],
-            required: true
-        },
-        level: {
-            type: Number,
-            required: true,
-            min: 1
-        }
-    }],
-    race: {
-        type: String,
-        enum: [
-            'Human',
-            'Elf',
-            'Half-Elf',
-            'Dwarf',
-            'Halfling',
-            'Gnome',
-            'Half-Orc',
-            'Dragonborn',
-            'Tiefling',
-            'Aasimar',
-            'Genasi',
-            'Goliath',
-            'Tabaxi',
-            'Triton',
-            'Firbolg',
-            'Kenku',
-            'Lizardfolk',
-            'Goblin',
-            'Orc',
-            'Bugbear'
-        ],
-        required: true
+      type: String,
+      enum: [
+        'Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk',
+        'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'
+      ],
+      required: true
     },
     level: {
-        type: Number,
-        required: true,
-        min: 1,
-        default: 1
-    },
-    stats: {
-        strength: { type: Number, required: true },
-        dexterity: { type: Number, required: true },
-        constitution: { type: Number, required: true },
-        intelligence: { type: Number, required: true },
-        wisdom: { type: Number, required: true },
-        charisma: { type: Number, required: true }
-    },
-    items: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Item'
-    }],
-    proficiencies: {
-        skills: [String],
-        armor: [String],
-        weapons: [String],
-        tools: [String]
-      },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    background: {
-        type: String,
-        required: true
-    },
-    backstory: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+      type: Number,
+      required: true,
+      min: 1
     }
+  }],
+  subclass: {
+    type: String,
+  },
+  race: {
+    type: String,
+    enum: [
+      'Human', 'Elf', 'Half-Elf', 'Dwarf', 'Halfling', 'Gnome',
+      'Half-Orc', 'Dragonborn', 'Tiefling', 'Aasimar', 'Genasi',
+      'Goliath', 'Tabaxi', 'Triton', 'Firbolg', 'Kenku', 'Lizardfolk',
+      'Goblin', 'Orc', 'Bugbear'
+    ],
+    required: true
+  },
+  level: {
+    type: Number,
+    required: true,
+    min: 1,
+    default: 1
+  },
+  stats: {
+    strength: { type: Number, required: true },
+    dexterity: { type: Number, required: true },
+    constitution: { type: Number, required: true },
+    intelligence: { type: Number, required: true },
+    wisdom: { type: Number, required: true },
+    charisma: { type: Number, required: true }
+  },
+  items: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Item'
+  }],
+  proficiencies: {
+    skills: [String],
+    armor: [String],
+    weapons: [String],
+    tools: [String]
+  },
+  spells: [{
+    name: String,
+    class: String
+  }],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  background: {
+    type: String,
+    required: true
+  },
+  backstory: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = model('Character', characterSchema);
