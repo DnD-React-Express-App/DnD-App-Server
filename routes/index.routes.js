010e6c246e6af6c4c3b04f773187cbb8163c7447
Dreng0t <louischicot4@gmail.com>
2025-05-28 09:10:42 +0100

    implemented health route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,21 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+router.get("/health", (req, res, next) => {
+  mongoose.connection.db.admin().ping()
+    .then(() => {
+      res.status(200).json({
+        status: 'ok',
+        timestamp: new Date.toISOString()
+      });
+    })
+    .catch(err => {
+      console.error("MongoDb ping failed", err);
+      res.status(500).json({
+        status: 'error',
+        message: "Failed to connect to MongoDB"
+      });
+    });
+});
+
 module.exports = router;
