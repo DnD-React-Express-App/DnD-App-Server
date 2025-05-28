d569151f36c1156f732d7c9cb541b577545a94c5
Dreng0t <louischicot4@gmail.com>
2025-05-28 10:34:30 +0100

    added netlify to CORS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: [FRONTEND_URL, 'https://dnd-app-client.netlify.app/'],
       credentials: true
     })
   );
